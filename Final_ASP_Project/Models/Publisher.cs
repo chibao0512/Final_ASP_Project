b07561f11ea2674001b5e554e99b71af0e420c60
mygcc20002 <ngodiemmy2211@gmail.com>
2023-03-04 17:41:06 +0700

    M: Publisher

---
+++ Final_ASP_Project/Models/Publisher.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Final_ASP_Project.Models
+{
+    public class Publisher
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Publisher_Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Publisher_Name { get; set; }
+        public string Hotline { get; set; }
+        public string Publisher_Address { get; set; }
+    }
+}
