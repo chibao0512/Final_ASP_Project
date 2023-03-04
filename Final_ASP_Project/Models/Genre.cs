c0d93ce7eeff401f6aa7eaa12c0131f50edd705e
mygcc20002 <ngodiemmy2211@gmail.com>
2023-03-04 17:40:46 +0700

    M: Genre

--- Final_ASP_Project/Models/Genre.cs
+++ Final_ASP_Project/Models/Genre.cs
@@ -8,16 +8,11 @@ namespace Final_ASP_Project.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Gen_Id { get; set; }
         [Required]
-        public int genre_Id { get; set; }
-        [Required]
-        [Display(Name = "genre_Name")]
-        public string genre_Name { get; set; }
-        [Required]
-        [Display(Name = "genre_Description")]
-        public string genre_Description { get; set; }
-        public string? genre_Status { get; set; }
+        public string Gen_Name { get; set; }
+        public string Gen_Description { get; set; }
+        public virtual ICollection<Book>? books { get; set; }
 
-        public virtual ICollection<Book>? Books { get; set; }
     }
 }
